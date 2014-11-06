26120495d29b7265bcfb9fbca401e26984d886ca
Johnson Yeap <johnson.runningbus@gmail.com>
2014-11-07 00:28:20 +0800

    Create User model validation and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+	def index
+	end
+	
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            # success
+            flash[:success] = "Your account has been successfully created"
+            redirect_to root_path
+        else
+            # fail
+            flash[:danger] = "Please try again"
+            render 'new'
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
 end
