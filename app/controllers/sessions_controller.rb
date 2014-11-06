7da192f39b94acda62afd3959e6f4ab99cf4744e
Johnson Yeap <johnson.runningbus@gmail.com>
2014-11-07 00:54:42 +0800

    Create Sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      # login
+      session[:user_id] = @user.id
+      flash[:success] = 'You have successfully signed in!'
+      redirect_to users_path
+    else
+      # incorrect password or username combination
+      flash.now[:danger] = 'Invalid username or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:success] = 'Successfully logged out'
+    redirect_to root_path
+  end
 end
