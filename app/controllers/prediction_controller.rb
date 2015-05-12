e5c3de6e4eaf5bb1875bd7e166304b6cf60dec49
Johnson Yeap <johnson.runningbus@gmail.com>
2015-05-12 23:48:46 +0800

    Query for the solutions and show the number of solutions

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -2,5 +2,6 @@ class PredictionController < ApplicationController
   def index
   	search = SimpleGa::GeneticAlgorithm::GeneticSearch.new(1000, 500) # new instance
   	@result = search.run
+  	@solutions = search.unique_solutions
   end
 end
