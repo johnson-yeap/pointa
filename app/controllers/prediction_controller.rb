118b640ef014dfcf00a1456a9857b3b284cf7da2
Johnson Yeap <johnson.runningbus@gmail.com>
2015-05-16 16:18:33 +0800

    Populate the datatable with data

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -3,7 +3,7 @@ class PredictionController < ApplicationController
   	available_courses = [["a",2],["b",3],["c",3],["d",3],["e",5],["f",2],["g",4],["h",4],["i",4],["j",4],["k",3]]
   	SimpleGa::GeneticAlgorithm::Chromosome.set_params(available_courses, 58, 16)
 
-  	search = SimpleGa::GeneticAlgorithm::GeneticSearch.new(100, 200) # new instance
+  	search = SimpleGa::GeneticAlgorithm::GeneticSearch.new(1000, 100) # new instance
   	@results = search.run
   	@best_result = search.best_chromosome
   end
