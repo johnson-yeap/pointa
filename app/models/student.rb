14a334133981ad7b333acccabc565f7d4edce745
Johnson Yeap <johnson.runningbus@gmail.com>
2015-05-17 01:50:00 +0800

    Add Remove option in the enrollment form

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,11 @@ class Student < ActiveRecord::Base
 	belongs_to :department
 	has_one :user
 
-	has_many :enrollments, dependent: :destroy
+	# dependent :destroy - when we destroy the student, the enrollments will be 
+	# destroyed as well
+	has_many :enrollments, dependent: :destroy 
   	has_many :courses, through: :enrollments
   	has_many :academic_year_semesters, -> { distinct }, through: :enrollments
 
-  	accepts_nested_attributes_for :enrollments
+  	accepts_nested_attributes_for :enrollments, allow_destroy: true
 end
