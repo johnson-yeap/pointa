52469769bd9aa11582b6b98c4c48794d18616e70
Johnson Yeap <johnson.runningbus@gmail.com>
2015-05-24 17:17:25 +0800

    Restrict the dean list to last semester only

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -8,7 +8,8 @@ class AnalysisController < ApplicationController
   	end
 
   	def deanlist
-	  	@session = AcademicYearSemester.find_by_id(params[:id]) || AcademicYearSemester.last
+	  	# @session = AcademicYearSemester.find_by_id(params[:id]) || AcademicYearSemester.last
+      @session = AcademicYearSemester.last(2).first
 			@students = Student.joins(:enrollments).where(enrollments: {academic_year_semester_id: @session.id}).uniq
       @bachelors = Bachelor.all
   	end
